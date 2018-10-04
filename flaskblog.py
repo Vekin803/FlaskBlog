7a56348f7fc2b82e60e2e28a1686631d3a2af638
Vekin803 <KiNd138877>
2018-10-04 14:52:28 +0200

    flash message

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -19,7 +19,8 @@ def hello():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!')
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title= 'Register', form=form)
 
 @app.route("/login")
