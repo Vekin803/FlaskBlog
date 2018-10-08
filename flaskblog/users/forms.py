64c59fd52f4c3ef5c0d57080a6e2fd3db577e03a
Vekin803 <KiNd138877>
2018-10-08 14:41:16 +0200

    Login required to acces, logo in login, eliminate new post link and side bar

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -30,7 +30,9 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+                        # render_kw={"placeholder": "Email"}
     password = PasswordField('Password', validators=[DataRequired()])
+                             # render_kw={"placeholder": "Password"}
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
